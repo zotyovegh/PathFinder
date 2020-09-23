8fc5a4102438c86c38614ceec5d449c039e2a953
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-09-23 23:23:23 +0200

    Small change

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import GridRow from "../GridRow";
 import "./index.css";
 
+const START_NODE_ROW = 10;
+const START_NODE_COL = 4;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 25;
+
 class Grid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rows: this.createGrid(props),
+      grid: this.createGrid(props),
     };
   }
 
@@ -21,21 +26,23 @@ class Grid extends Component {
           y: i,
           start: false,
           end: false,
+          distance: Infinity,
+          isWall: false,
         });
       }
     }
 
-    grid[10][4].start = true;
-    grid[10][25].end = true;
+    grid[START_NODE_ROW][START_NODE_COL].start = true;
+    grid[FINISH_NODE_ROW][FINISH_NODE_COL].end = true;
 
     return grid;
   };
 
   render() {
-    let rows = this.state.rows.map((row, index) => {
+    let grid = this.state.grid.map((row, index) => {
       return <GridRow cells={row} key={index} />;
     });
-    return <div className="grid">{rows}</div>;
+    return <div className="grid">{grid}</div>;
   }
 }
 
