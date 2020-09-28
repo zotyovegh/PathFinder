86543aa6792fbdd2309cb8af19c66be3199439a2
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-09-28 21:38:11 +0200

    Simplified cell management

--- src/Cell/index.js
+++ src/Cell/index.js
@@ -1,98 +1,37 @@
 import React from "react";
 import "./index.css";
-
+function getColor(props) {
+  if (props.data.start) {
+    return "start";
+  } else if (props.data.end) {
+    return "end";
+  } else if (props.data.isWall) {
+    return "wall";
+  } else if (props.data.visited) {
+    return "visited";
+  } else {
+    return "";
+  }
+}
 const Cell = (props) => {
   let cell = () => {
-    if (props.data.start) {
-      return (
-        <div
-          className="cell"
-          id="start"
-          onMouseDown={(e) => {
-            e.preventDefault();
-            props.onMouseDown(props.data);
-          }}
-          onMouseEnter={() => props.onMouseEnter(props.data)}
-          onMouseUp={() => props.onMouseUp(props.data)}
-          onContextMenu={(e) => {
-            e.preventDefault();
-          }}
-        >
-          {" "}
-        </div>
-      );
-    } else if (props.data.end) {
-      return (
-        <div
-          className="cell"
-          id="end"
-          onMouseDown={(e) => {
-            e.preventDefault();
-            props.onMouseDown(props.data);
-          }}
-          onMouseEnter={() => props.onMouseEnter(props.data)}
-          onMouseUp={() => props.onMouseUp(props.data)}
-          onContextMenu={(e) => {
-            e.preventDefault();
-          }}
-        >
-          {" "}
-        </div>
-      );
-    } else if (props.data.isWall) {
-      return (
-        <div
-          className="cell"
-          id="wall"
-          onMouseDown={(e) => {
-            e.preventDefault();
-            props.onMouseDown(props.data);
-          }}
-          onMouseEnter={() => props.onMouseEnter(props.data)}
-          onMouseUp={() => props.onMouseUp(props.data)}
-          onContextMenu={(e) => {
-            e.preventDefault();
-          }}
-        >
-          {" "}
-        </div>
-      );
-    } else if (props.data.visited) {
-      return (
-        <div
-          className="cell"
-          id="visited"
-          onMouseDown={(e) => {
-            e.preventDefault();
-            props.onMouseDown(props.data);
-          }}
-          onMouseEnter={() => props.onMouseEnter(props.data)}
-          onMouseUp={() => props.onMouseUp(props.data)}
-          onContextMenu={(e) => {
-            e.preventDefault();
-          }}
-        >
-          {" "}
-        </div>
-      );
-    } else {
-      return (
-        <div
-          className="cell"
-          onMouseDown={(e) => {
-            e.preventDefault();
-            props.onMouseDown(props.data);
-          }}
-          onMouseEnter={() => props.onMouseEnter(props.data)}
-          onMouseUp={() => props.onMouseUp(props.data)}
-          onContextMenu={(e) => {
-            e.preventDefault();
-          }}
-        >
-          {" "}
-        </div>
-      );
-    }
+    return (
+      <div
+        className="cell"
+        id={getColor(props)}
+        onMouseDown={(e) => {
+          e.preventDefault();
+          props.onMouseDown(props.data);
+        }}
+        onMouseEnter={() => props.onMouseEnter(props.data)}
+        onMouseUp={() => props.onMouseUp(props.data)}
+        onContextMenu={(e) => {
+          e.preventDefault();
+        }}
+      >
+        {" "}
+      </div>
+    );
   };
   return cell();
 };
