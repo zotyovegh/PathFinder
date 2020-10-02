8ce12d5992e9dbca2360aa6152b889a32723f6e5
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-10-03 00:09:15 +0200

    Endpoint positioning to be based on the grid size

--- src/App.js
+++ src/App.js
@@ -7,20 +7,42 @@ class App extends Component {
     this.state = {
       rows: this.getSize("rows"),
       columns: this.getSize("cols"),
+      startR: this.getPosition("row"),
+      startC: this.getPosition("startCol"),
+      endR: this.getPosition("row"),
+      endC: this.getPosition("endCol"),
     };
   }
+
+  getPosition = (parameter) => {
+    let position = null;
+    if (parameter === "row") {
+      position = Math.floor(this.getSize("rows") / 2);
+    } else if (parameter === "startCol") {
+      position = Math.floor(this.getSize("cols") / 5);
+    } else if (parameter === "endCol") {
+      let x = this.getSize("cols");
+      let margin = Math.ceil(x / 5);
+      position = x - margin;
+    }
+
+    return position;
+  };
 
   getSize = (type) => {
     let size = null;
     if (type === "rows") {
       size = (window.innerHeight / 30).toFixed(0) - 5;
-      return size;
+      if (size < 10) {
+        return 10;
+      }
     } else if (type === "cols") {
       size = (window.innerWidth / 30).toFixed(0) - 5;
-      return size;
-    } else {
-      return 20;
+      if (size < 10) {
+        return 10;
+      }
     }
+    return size;
   };
 
   render() {
@@ -29,10 +51,10 @@ class App extends Component {
         <Grid
           rows={this.state.rows}
           columns={this.state.columns}
-          startR={10}
-          startC={4}
-          endR={10}
-          endC={25}
+          startR={this.state.startR}
+          startC={this.state.startC}
+          endR={this.state.endR}
+          endC={this.state.endC}
         />
       </div>
     );
