2805e778cf15da35f16b8407978419db02a92289
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-10-02 23:40:29 +0200

    Grid sizing by window height and width

--- src/App.js
+++ src/App.js
@@ -5,15 +5,35 @@ class App extends Component {
   constructor(props) {
     super();
     this.state = {
-      rows: 21,
-      columns: 30,
+      rows: this.getSize("rows"),
+      columns: this.getSize("cols"),
     };
   }
+
+  getSize = (type) => {
+    let size = null;
+    if (type === "rows") {
+      size = (window.innerHeight / 30).toFixed(0) - 5;
+      return size;
+    } else if (type === "cols") {
+      size = (window.innerWidth / 30).toFixed(0) - 5;
+      return size;
+    } else {
+      return 20;
+    }
+  };
 
   render() {
     return (
       <div className="App">
-        <Grid rows={this.state.rows} columns={this.state.columns} startR={10} startC={4}endR={10} endC={25}/>
+        <Grid
+          rows={this.state.rows}
+          columns={this.state.columns}
+          startR={10}
+          startC={4}
+          endR={10}
+          endC={25}
+        />
       </div>
     );
   }
