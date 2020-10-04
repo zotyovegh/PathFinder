37b0ac0b1844d0c4b1c23a1b9739493afb6d4007
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-10-04 20:36:31 +0200

    Updates

---
+++ precache-manifest.6b9b14a9c52d642edefb591188cee6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b097320c6a8948136c909383e5a797",
+    "url": "/PathFinder/index.html"
+  },
+  {
+    "revision": "32e47b857f4a77269b71",
+    "url": "/PathFinder/static/css/main.fdb6cecb.chunk.css"
+  },
+  {
+    "revision": "b982cd4a76c0cb94be4a",
+    "url": "/PathFinder/static/js/2.a46fce13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PathFinder/static/js/2.a46fce13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e47b857f4a77269b71",
+    "url": "/PathFinder/static/js/main.89754d33.chunk.js"
+  },
+  {
+    "revision": "a4b934835c08b6dd6b90",
+    "url": "/PathFinder/static/js/runtime-main.519de40d.js"
+  }
+]);
