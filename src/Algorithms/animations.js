2d3558a6d6bccfe3b1a172048faef247d9c25f77
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-10-20 23:22:38 +0200

    Ordering in animations.js

--- src/Algorithms/animations.js
+++ src/Algorithms/animations.js
@@ -1,5 +1,51 @@
 import { visualizeCell, clearVisitedCells } from "../Algorithms/methods";
 
+export function animateSlow(visitedCells, cellsInOrder) {
+  for (let i = 0; i <= visitedCells.length; i++) {
+    const cell = visitedCells[i];
+    if (i === visitedCells.length) {
+      setTimeout(() => {
+        animatePathSlow(cellsInOrder);
+      }, 10 * i);
+      return;
+    }
+    setTimeout(() => {
+      if (cell.start && window.gridComponent.state.previousVisualization) {
+        visualizeCell("num", "num num-start", cell);
+      }
+      if (cell.end && window.gridComponent.state.previousVisualization) {
+        visualizeCell("num", "num num-end", cell);
+      }
+      if (!cell.isWall) {
+        if (!cell.start && !cell.end) {
+          visualizeCell("cell", "cell cell-visited-animated", cell);
+          if (window.gridComponent.state.previousVisualization) {
+            visualizeCell("num", "num num-visited", cell);
+          }
+        }
+      }
+    }, 10 * i);
+  }
+}
+
+function animatePathSlow(cellsInOrder) {
+  for (let i = 0; i <= cellsInOrder.length; i++) {
+    setTimeout(() => {
+      if (i === cellsInOrder.length) {
+        window.gridComponent.setState({ status: "finished" });
+        return;
+      }
+      const cell = cellsInOrder[i];
+      if (!cell.start && !cell.end) {
+        visualizeCell("cell", "cell cell-path-animated", cell);
+        if (window.gridComponent.state.previousVisualization) {
+          visualizeCell("num", "num num-path", cell);
+        }
+      }
+    }, 20 * i);
+  }
+}
+
 export function animateFast(visitedCells, cellsInOrder) {
   for (let i = 0; i <= visitedCells.length - 1; i++) {
     const cell = visitedCells[i];
@@ -17,6 +63,18 @@ export function animateFast(visitedCells, cellsInOrder) {
         if (window.gridComponent.state.previousVisualization) {
           visualizeCell("num", "num num-visited", cell);
         }
+      }
+    }
+  }
+}
+
+function animatePathFast(cellsInOrder) {
+  for (let i = 0; i < cellsInOrder.length; i++) {
+    const cell = cellsInOrder[i];
+    if (!cell.start && !cell.end) {
+      visualizeCell("cell", "cell cell-path", cell);
+      if (window.gridComponent.state.previousVisualization) {
+        visualizeCell("num", "num num-path", cell);
       }
     }
   }
@@ -73,63 +131,4 @@ export function animateAstarFast(allSet, openSet, cellsInOrder) {
     }
   }
   animatePathFast(cellsInOrder);
-}
-
-function animatePathFast(cellsInOrder) {
- 
-  for (let i = 0; i < cellsInOrder.length; i++) {
-    const cell = cellsInOrder[i];
-    if (!cell.start && !cell.end) {
-      visualizeCell("cell", "cell cell-path", cell);
-      if (window.gridComponent.state.previousVisualization) {
-        visualizeCell("num", "num num-path", cell);
-      }
-    }
-  }
-}
-
-export function animateSlow(visitedCells, cellsInOrder) {
-  for (let i = 0; i <= visitedCells.length; i++) {
-    const cell = visitedCells[i];
-    if (i === visitedCells.length) {
-      setTimeout(() => {
-        animatePathSlow(cellsInOrder);
-      }, 10 * i);
-      return;
-    }
-    setTimeout(() => {
-      if (cell.start && window.gridComponent.state.previousVisualization) {
-        visualizeCell("num", "num num-start", cell);
-      }
-      if (cell.end && window.gridComponent.state.previousVisualization) {
-        visualizeCell("num", "num num-end", cell);
-      }
-      if (!cell.isWall) {
-        if (!cell.start && !cell.end) {
-          visualizeCell("cell", "cell cell-visited-animated", cell);
-          if (window.gridComponent.state.previousVisualization) {
-            visualizeCell("num", "num num-visited", cell);
-          }
-        }
-      }
-    }, 10 * i);
-  }
-}
-
-function animatePathSlow(cellsInOrder) {
-  for (let i = 0; i <= cellsInOrder.length; i++) {
-    setTimeout(() => {
-      if (i === cellsInOrder.length) {
-        window.gridComponent.setState({ status: "finished" });
-        return;
-      }
-      const cell = cellsInOrder[i];
-      if (!cell.start && !cell.end) {
-        visualizeCell("cell", "cell cell-path-animated", cell);
-        if (window.gridComponent.state.previousVisualization) {
-          visualizeCell("num", "num num-path", cell);
-        }
-      }
-    }, 20 * i);
-  }
 }
