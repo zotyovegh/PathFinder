e4f9428c13f520a35d65dcbe524bf95c2539be0e
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-10-25 22:14:26 +0100

    function for getting neighboring walls

--- src/Algorithms/mazes/prim.js
+++ src/Algorithms/mazes/prim.js
@@ -1,3 +1,39 @@
 export function primMaze(grid) {
-  console.log("prim");
+  //var grid = JSON.parse(JSON.stringify(originalGrid));
+  const validWalls = [];
+
+  for (const row of grid) {
+    for (const cell of row) {
+      cell.isWall = true;
+    }
+  }
+  const mazeCells = [];
+  grid[0][0].isWall = false;
+  mazeCells.push(grid[0][0]);
+  getNeighboringWalls(grid[0][0], grid, validWalls);
+  console.log(validWalls);
+}
+
+function getNeighboringWalls(cell, grid, validWalls) {
+  var { col, row } = cell;
+  if (row > 0) {
+    //UP
+    var cell = grid[row - 1][col];
+    validWalls.push(cell);
+  }
+  if (col < grid[0].length - 1) {
+    //Right
+    let cell = grid[row][col + 1];
+    validWalls.push(cell);
+  }
+  if (row < grid.length - 1) {
+    //Down
+    let cell = grid[row + 1][col];
+    validWalls.push(cell);
+  }
+  if (col > 0) {
+    //Left
+    let cell = grid[row][col - 1];
+    validWalls.push(cell);
+  }
 }
