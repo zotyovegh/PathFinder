ac1335fa8bdd4efb865e30bdf3b4b1826c059b28
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-10-29 21:54:34 +0100

    Iterative maze #1

--- src/Algorithms/mazes/iterative.js
+++ src/Algorithms/mazes/iterative.js
@@ -1,3 +1,10 @@
+import { clearWithStatus } from "../../Algorithms/cleaning";
 export function iterativeMaze(originalGrid) {
-
+  clearWithStatus("path");
+  var grid = JSON.parse(JSON.stringify(originalGrid));
+  for (const row of grid) {
+    for (const cell of row) {
+      cell.isWall = true;
+    }
+  }
 }
