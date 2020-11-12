1f1433631eff5ca75549d3f1fe5b6b8b2ee5e44c
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-11-12 23:35:22 +0100

    Remove unnecessary wrong lines from the mazes

--- src/Algorithms/mazes/iterative.js
+++ src/Algorithms/mazes/iterative.js
@@ -37,7 +37,7 @@ export function iterativeMaze(originalGrid) {
     }
   }
 
-   clearInfinityVariables(grid);
+  clearInfinityVariables(grid);
   window.gridComponent.setState({ grid: grid });
   visualizeOnWalledGrid(grid, path);
 }
@@ -54,40 +54,32 @@ function getNeighboringCells(cell, grid) {
   var { col, row } = cell;
   if (row > 1) {
     //UP
-
-    if (grid[row - 2][col].isWall) {
-      var neighbor = grid[row - 1][col];
-      if (grid[row - 2][col].visited === false) {
-        neighboringUnvisitedPairs.push([neighbor, grid[row - 2][col]]);
-      }
+    var neighbor = grid[row - 1][col];
+    if (grid[row - 2][col].visited === false) {
+      neighboringUnvisitedPairs.push([neighbor, grid[row - 2][col]]);
     }
   }
   if (col < grid[0].length - 2) {
     //Right
-    if (grid[row][col + 2].isWall) {
-      let neighbor = grid[row][col + 1];
-      if (grid[row][col + 2].visited === false) {
-        neighboringUnvisitedPairs.push([neighbor, grid[row][col + 2]]);
-      }
+    let neighbor = grid[row][col + 1];
+    if (grid[row][col + 2].visited === false) {
+      neighboringUnvisitedPairs.push([neighbor, grid[row][col + 2]]);
     }
   }
   if (row < grid.length - 2) {
     //Down
-    if (grid[row + 2][col]) {
-      let neighbor = grid[row + 1][col];
-      if (grid[row + 2][col].visited === false) {
-        neighboringUnvisitedPairs.push([neighbor, grid[row + 2][col]]);
-      }
+    let neighbor = grid[row + 1][col];
+    if (grid[row + 2][col].visited === false) {
+      neighboringUnvisitedPairs.push([neighbor, grid[row + 2][col]]);
     }
   }
   if (col > 1) {
     //Left
-    if (grid[row][col - 2]) {
-      let neighbor = grid[row][col - 1];
-      if (grid[row][col - 2].visited === false) {
-        neighboringUnvisitedPairs.push([neighbor, grid[row][col - 2]]);
-      }
+    let neighbor = grid[row][col - 1];
+    if (grid[row][col - 2].visited === false) {
+      neighboringUnvisitedPairs.push([neighbor, grid[row][col - 2]]);
     }
   }
+
   return neighboringUnvisitedPairs;
 }
--- src/Algorithms/mazes/prim.js
+++ src/Algorithms/mazes/prim.js
@@ -33,31 +33,23 @@ function getNeighboringWalls(cell, grid, wallPairs) {
   var { col, row } = cell;
   if (row > 1) {
     //UP
-    if (grid[row - 2][col].isWall) {
-      var neighbor = grid[row - 1][col];
-      wallPairs.push([neighbor, grid[row - 2][col]]);
-    }
+    var neighbor = grid[row - 1][col];
+    wallPairs.push([neighbor, grid[row - 2][col]]);
   }
   if (col < grid[0].length - 2) {
     //Right
-    if (grid[row][col + 2].isWall) {
-      let neighbor = grid[row][col + 1];
-      wallPairs.push([neighbor, grid[row][col + 2]]);
-    }
+    let neighbor = grid[row][col + 1];
+    wallPairs.push([neighbor, grid[row][col + 2]]);
   }
   if (row < grid.length - 2) {
     //Down
-    if (grid[row + 2][col]) {
-      let neighbor = grid[row + 1][col];
-      wallPairs.push([neighbor, grid[row + 2][col]]);
-    }
+    let neighbor = grid[row + 1][col];
+    wallPairs.push([neighbor, grid[row + 2][col]]);
   }
   if (col > 1) {
     //Left
-    if (grid[row][col - 2]) {
-      let neighbor = grid[row][col - 1];
-      wallPairs.push([neighbor, grid[row][col - 2]]);
-    }
+    let neighbor = grid[row][col - 1];
+    wallPairs.push([neighbor, grid[row][col - 2]]);
   }
 }
 
