15ae9502cb9491dbfd083691eef5d2daae19992b
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-11-15 22:54:54 +0100

    Kruskal #1

--- src/Algorithms/mazes/kruskal.js
+++ src/Algorithms/mazes/kruskal.js
@@ -4,5 +4,17 @@ import {
 } from "../../Algorithms/cleaning";
 import { visualizeOnWalledGrid } from "../mazes/animations";
 export function kruskalMaze(originalGrid) {
-  
+  clearWithStatus("path");
+  var path = [];
+  var grid = JSON.parse(JSON.stringify(originalGrid));
+  var idCounter = 0;
+  for (const row of grid) {
+    for (const cell of row) {
+      cell.isWall = true;
+      if (cell.row % 2 === 1 && cell.col % 2 === 1) {
+        cell.id = idCounter;
+        idCounter++;
+      }
+    }
+  }
 }
