dff808d37206c7fa1211c5f26f87686f0505665d
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-11-16 00:19:22 +0100

    Kruskal #3

--- src/Algorithms/mazes/kruskal.js
+++ src/Algorithms/mazes/kruskal.js
@@ -8,17 +8,33 @@ export function kruskalMaze(originalGrid) {
   var path = [];
   var grid = JSON.parse(JSON.stringify(originalGrid));
   var idCounter = 0;
-  var potentialCells = [];
+  // var potentialCells = [];
+  var validWalls = [];
+  var map = new Map();
   for (const row of grid) {
     for (const cell of row) {
       cell.isWall = true;
       if (cell.row % 2 === 1 && cell.col % 2 === 1) {
-        cell.id = idCounter;
-        idCounter++;
-        potentialCells.push(cell);
+        //CELLS
+        // potentialCells.push(cell);
+
+        var values = map.get(cell);
+        if (values) values.push(cell);
+        else map.set(idCounter++, [cell]);
+      } else if (cell.row % 2 === 1 || cell.col % 2 === 1) {
+        if (
+          cell.row !== 0 &&
+          cell.col !== 0 &&
+          cell.row !== grid.length - 1 &&
+          cell.col !== grid[0].length - 1
+        ) {
+          //WALLS
+          // validWalls.push(cell);
+        }
       }
     }
   }
+  console.log(map);
 
-  visualize(potentialCells);
+  visualize(validWalls);
 }
