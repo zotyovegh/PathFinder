793a16f50be60e216755210bf6b7ddb219582861
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-11-24 21:28:28 +0100

    RecursiveDivision #1

---
+++ src/Algorithms/mazes/recursiveDivision.js
@@ -0,0 +1,24 @@
+import {
+  clearWithStatus,
+  clearInfinityVariables,
+} from "../cleaning";
+import { visualizeOnWalledGrid } from "./animations";
+export function recursiveMaze(originalGrid) {
+  clearWithStatus("path");
+  var path = [];
+  var grid = JSON.parse(JSON.stringify(originalGrid));
+  for (const row of grid) {
+    for (const cell of row) {
+      cell.isWall = true;
+    }
+  }
+  /*var currentCell = grid[1][1];
+  recursion(grid, currentCell);
+  clearInfinityVariables(grid);
+  window.gridComponent.setState({ grid: grid }); 
+  visualizeOnWalledGrid(grid, path);*/
+}
+
+function recursion(grid, currentCell) {
+ 
+}
