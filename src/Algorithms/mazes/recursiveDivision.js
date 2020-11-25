8a26fb8675849f49504635ec72a0b5aa8623af00
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-11-25 23:58:57 +0100

    RD #4

--- src/Algorithms/mazes/recursiveDivision.js
+++ src/Algorithms/mazes/recursiveDivision.js
@@ -14,18 +14,37 @@ export function recursiveDivision(originalGrid) {
     grid[1][grid[1].length - 2],
     grid[grid.length - 2][1],
     grid[grid.length - 2][grid[1].length - 2],
-    grid, path
+    grid,
+    path
   );
- 
-  
- /* clearInfinityVariables(grid);
+
+  /* clearInfinityVariables(grid);
   window.gridComponent.setState({ grid: grid });*/
   visualizeRD(grid, path);
 }
 
 function recursion(topLeft, topRight, bottomLeft, bottomRight, grid, path) {
-   path.push(topLeft);
+  path.push(topLeft);
   path.push(topRight);
   path.push(bottomLeft);
   path.push(bottomRight);
+  var orientation = "asd";
+  var width = topRight.col - topLeft.col + 1;
+  var height = bottomLeft.row - topLeft.row + 1;
+  if (width < 2 || height < 2) return;
+  if (width >= height) {
+    orientation = "vertical";
+    var half = Math.ceil(width / 2);
+    var middle = half % 2 === 0 ? half : half + (Math.random() < 0.5 ? -1 : 1);
+    for (let i = topLeft.row; i < topLeft.row + height; i++) {
+      path.push(grid[i][middle]);
+    }
+  } else {
+    orientation = "horizontal";
+    var half = Math.ceil(height / 2);
+    var middle = half % 2 === 0 ? half : half + (Math.random() < 0.5 ? -1 : 1);
+    for (let i = topLeft.col; i < topLeft.col + width; i++) {
+      path.push(grid[middle][i]);
+    }
+  }
 }
