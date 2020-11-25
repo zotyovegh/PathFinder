015bce9d41fbbeeb090b98ecb6734010d31dddd5
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-11-26 00:21:43 +0100

    RD #5

--- src/Algorithms/mazes/recursiveDivision.js
+++ src/Algorithms/mazes/recursiveDivision.js
@@ -36,14 +36,23 @@ function recursion(topLeft, topRight, bottomLeft, bottomRight, grid, path) {
     orientation = "vertical";
     var half = Math.ceil(width / 2);
     var middle = half % 2 === 0 ? half : half + (Math.random() < 0.5 ? -1 : 1);
+    var num = Math.floor(Math.random() * (height - 1 + 1)) + 1;
+    var randomPosition =
+      num % 2 === 1 ? num : num + (Math.random() < 0.5 ? -1 : 1);
     for (let i = topLeft.row; i < topLeft.row + height; i++) {
+      if (i === topLeft.row + randomPosition - 1) continue;
       path.push(grid[i][middle]);
     }
   } else {
     orientation = "horizontal";
     var half = Math.ceil(height / 2);
     var middle = half % 2 === 0 ? half : half + (Math.random() < 0.5 ? -1 : 1);
+    var num = Math.floor(Math.random() * (width - 1 + 1)) + 1;
+    var randomPosition =
+      num % 2 === 1 ? num : num + (Math.random() < 0.5 ? -1 : 1);
+
     for (let i = topLeft.col; i < topLeft.col + width; i++) {
+      if (i === topLeft.col + randomPosition - 1) continue;
       path.push(grid[middle][i]);
     }
   }
