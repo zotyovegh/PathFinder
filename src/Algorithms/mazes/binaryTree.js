c82ed9cadd4a27468bf28d199e2f7a2e9c20b965
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-11-29 19:41:30 +0100

    Bt #3

--- src/Algorithms/mazes/binaryTree.js
+++ src/Algorithms/mazes/binaryTree.js
@@ -9,8 +9,43 @@ export function binaryTreeAlg(originalGrid) {
       cell.isWall = true;
     }
   }
+  path.push(grid[3][3]);
+  getNeighboringCells(grid[3][3], grid, path);
 
   clearInfinityVariables(grid);
   window.gridComponent.setState({ grid: grid });
   visualizeOnWalledGrid(grid, path);
+}
+
+function getNeighboringCells(cell, grid, path, param1, param2) {
+  //pair[neighboringWall, neighbor]
+  var { col, row } = cell;
+  if (row > 1) {
+    //UP
+    var neighbor = grid[row - 1][col];
+    cell.neighbors.push([neighbor, grid[row - 2][col]]);
+    path.push(neighbor);
+    path.push(grid[row - 2][col]);
+  }
+  if (col < grid[0].length - 2) {
+    //Right
+    let neighbor = grid[row][col + 1];
+    cell.neighbors.push([neighbor, grid[row][col + 2]]);
+    path.push(neighbor);
+    path.push(grid[row][col + 2]);
+  }
+  if (row < grid.length - 2) {
+    //Down
+    let neighbor = grid[row + 1][col];
+    cell.neighbors.push([neighbor, grid[row + 2][col]]);
+    path.push(neighbor);
+    path.push(grid[row + 2][col]);
+  }
+  if (col > 1) {
+    //Left
+    let neighbor = grid[row][col - 1];
+    cell.neighbors.push([neighbor, grid[row][col - 2]]);
+    path.push(neighbor);
+    path.push(grid[row][col - 2]);
+  }
 }
