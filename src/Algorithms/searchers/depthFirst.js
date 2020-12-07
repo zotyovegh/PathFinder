907d760acd853f3325437c10a113598a9ca04654
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-12-07 23:31:12 +0100

    DF Big update - almost the whole alg implemented, the result is strange
    Every second line processed only

--- src/Algorithms/searchers/depthFirst.js
+++ src/Algorithms/searchers/depthFirst.js
@@ -3,15 +3,78 @@ import { getCellsInOrder } from "../../Algorithms/methods";
 import { clearVisitedCells } from "../../Algorithms/cleaning";
 var id = 0;
 export function depthFirst(grid, startCell, endCell, isDiagonalOn, speed) {
-  
-  const unvisitedCells = [];
-  for (const row of grid) {
-    for (const cell of row) {
-      unvisitedCells.push(cell);
+  console.log("heyy");
+  var mainList = [];
+  var visitedCells = [];
+  startCell.visited = true;
+  mainList.push(startCell);
+  visitedCells.push(startCell);
+  var neighbors = getUnvisitedNeighbors(startCell, grid);
+  console.log(neighbors);
+
+  while (!!mainList.length) {
+    const currentCell = mainList.pop();
+    visitedCells.push(currentCell);
+
+    if (currentCell.end === true) {
+      DoAnimation(visitedCells, endCell, speed);
+      return;
+    }
+    var neighbors = getUnvisitedNeighbors(currentCell, grid);
+    for (let i = 0; i < neighbors.length; i++) {
+      neighbors[i].visited = true;
+      neighbors[i].previous = currentCell;
+      mainList.push(neighbors[i]);
     }
   }
+}
 
-  // while (!!unvisitedCells.length) {}
+function getUnvisitedNeighbors(cell, grid) {
+  const neighbors = [];
+  var { col, row } = cell;
+
+  Left(row, col, grid, neighbors);
+  Up(row, col, grid, neighbors);
+  Right(row, col, grid, neighbors);
+  Down(row, col, grid, neighbors);
+
+  return neighbors;
+}
+
+function Up(row, col, grid, neighbors) {
+  if (row > 0) {
+    var cell = grid[row - 1][col];
+    if (!cell.visited && cell.previous === null) {
+      neighbors.push(cell);
+    }
+  }
+}
+
+function Right(row, col, grid, neighbors) {
+  if (col < grid[0].length - 1) {
+    let cell = grid[row][col + 1];
+    if (!cell.visited && cell.previous === null) {
+      neighbors.push(cell);
+    }
+  }
+}
+
+function Down(row, col, grid, neighbors) {
+  if (row < grid.length - 1) {
+    let cell = grid[row + 1][col];
+    if (!cell.visited && cell.previous === null) {
+      neighbors.push(cell);
+    }
+  }
+}
+
+function Left(row, col, grid, neighbors) {
+  if (col > 0) {
+    let cell = grid[row][col - 1];
+    if (!cell.visited && cell.previous === null) {
+      neighbors.push(cell);
+    }
+  }
 }
 
 function DoAnimation(visitedCells, endCell, speed) {
