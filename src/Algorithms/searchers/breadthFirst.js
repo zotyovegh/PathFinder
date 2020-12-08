d9c0bfd1bd4a7d13c90762b40a916b8e478762ca
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-12-08 19:07:59 +0100

    BF big update, small issues occurred, result is correct

--- src/Algorithms/searchers/breadthFirst.js
+++ src/Algorithms/searchers/breadthFirst.js
@@ -3,5 +3,85 @@ import { getCellsInOrder } from "../../Algorithms/methods";
 import { clearVisitedCells } from "../../Algorithms/cleaning";
 var id = 0;
 export function breadthFirst(grid, startCell, endCell, speed) {
-  console.log("heyy");
+  var mainList = [];
+  var visitedCells = [];
+  startCell.visited = true;
+  mainList.push(startCell);
+  visitedCells.push(startCell);
+
+  while (!!mainList.length) {
+    const currentCell = mainList.shift();
+    visitedCells.push(currentCell);
+
+    if (currentCell.end === true) {
+      DoAnimation(visitedCells, endCell, speed);
+      return;
+    }
+    var neighbors = getUnvisitedNeighbors(currentCell, grid);
+    for (let i = 0; i < neighbors.length; i++) {
+      neighbors[i].visited = true;
+      neighbors[i].previous = currentCell;
+      mainList.push(neighbors[i]);
+    }
+  }
+}
+
+function getUnvisitedNeighbors(cell, grid) {
+  const neighbors = [];
+  var { col, row } = cell;
+  Up(row, col, grid, neighbors);
+  Right(row, col, grid, neighbors);
+  Down(row, col, grid, neighbors);
+  Left(row, col, grid, neighbors);
+  return neighbors;
+}
+
+function Up(row, col, grid, neighbors) {
+  if (row > 0) {
+    var cell = grid[row - 1][col];
+    if (!cell.visited) {
+      neighbors.push(cell);
+    }
+  }
+}
+
+function Right(row, col, grid, neighbors) {
+  if (col < grid[0].length - 1) {
+    let cell = grid[row][col + 1];
+    if (!cell.visited) {
+      neighbors.push(cell);
+    }
+  }
+}
+
+function Down(row, col, grid, neighbors) {
+  if (row < grid.length - 1) {
+    let cell = grid[row + 1][col];
+    if (!cell.visited) {
+      neighbors.push(cell);
+    }
+  }
+}
+
+function Left(row, col, grid, neighbors) {
+  if (col > 0) {
+    let cell = grid[row][col - 1];
+    if (!cell.visited) {
+      neighbors.push(cell);
+    }
+  }
+}
+
+function DoAnimation(visitedCells, endCell, speed) {
+  const cellsInOrder = getCellsInOrder(endCell);
+  if (speed === "slow") {
+    if (window.gridComponent.state.status === "finished") {
+      clearVisitedCells();
+    }
+    window.gridComponent.setState({ status: "running" });
+
+    animateSlow(visitedCells, cellsInOrder);
+  } else if (speed === "fast") {
+    animateFast(visitedCells, cellsInOrder);
+  }
 }
