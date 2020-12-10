a088b7532ee5d12301724067837f73a0b1916dc6
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-12-10 18:29:47 +0100

    Idastar searching alg. preparation

---
+++ src/Algorithms/idastar.js
@@ -0,0 +1,94 @@
+import { animateAstarSlow, animateAstarFast } from "./animations";
+import { getCellsInOrder } from "../../Algorithms/methods";
+import { clearVisitedCells } from "../../Algorithms/cleaning";
+
+export function idastar(
+  grid,
+  startCell,
+  endCell,
+  isDiagonalOn,
+
+  speed
+) {
+  findNeighbors(grid, isDiagonalOn);
+
+  DoAnimation(allSet, openSet, endCell, speed);
+}
+
+function findNeighbors(grid, isDiagonalOn) {
+  for (const row of grid) {
+    for (const cell of row) {
+      cell.neighbors = [];
+      if (cell.row > 0) {
+        //UP
+        cell.neighbors.push(grid[cell.row - 1][cell.col]);
+      }
+      if (cell.col < grid[0].length - 1) {
+        //RIGHT
+        cell.neighbors.push(grid[cell.row][cell.col + 1]);
+      }
+
+      if (cell.row < grid.length - 1) {
+        //DOWN
+        cell.neighbors.push(grid[cell.row + 1][cell.col]);
+      }
+
+      if (cell.col > 0) {
+        //LEFT
+        cell.neighbors.push(grid[cell.row][cell.col - 1]);
+      }
+      if (isDiagonalOn) {
+        if (cell.row > 0 && cell.col < grid[0].length - 1) {
+          //UPRIGHT
+          if (
+            !grid[cell.row - 1][cell.col].isWall ||
+            !grid[cell.row][cell.col + 1].isWall
+          ) {
+            cell.neighbors.push(grid[cell.row - 1][cell.col + 1]);
+          }
+        }
+        if (cell.col < grid[0].length - 1 && cell.row < grid.length - 1) {
+          //RIGHTDOWN
+          if (
+            !grid[cell.row + 1][cell.col].isWall ||
+            !grid[cell.row][cell.col + 1].isWall
+          ) {
+            cell.neighbors.push(grid[cell.row + 1][cell.col + 1]);
+          }
+        }
+        if (cell.row < grid.length - 1 && cell.col > 0) {
+          //DOWNLEFT
+          if (
+            !grid[cell.row + 1][cell.col].isWall ||
+            !grid[cell.row][cell.col - 1].isWall
+          ) {
+            cell.neighbors.push(grid[cell.row + 1][cell.col - 1]);
+          }
+        }
+        if (cell.col > 0 && cell.row > 0) {
+          //LEFTUP
+          if (
+            !grid[cell.row][cell.col - 1].isWall ||
+            !grid[cell.row - 1][cell.col].isWall
+          ) {
+            cell.neighbors.push(grid[cell.row - 1][cell.col - 1]);
+          }
+        }
+      }
+    }
+  }
+}
+
+function DoAnimation(allSet, openSet, endCell, speed) {
+  const cellsInOrder = getCellsInOrder(endCell);
+  if (speed === "slow") {
+    if (window.gridComponent.state.status === "finished") {
+      clearVisitedCells();
+    }
+    window.gridComponent.setState({ status: "running" });
+
+    animateAstarSlow(allSet, cellsInOrder);
+  } else if (speed === "fast") {
+    animateAstarFast(allSet, openSet, cellsInOrder);
+  }
+}
