94cba6e18f27511efc90f13cccafad7660d43d32
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-12-13 20:39:03 +0100

    Endpoint number visualization bug fixed, bidirectional on dijkstra is finished

--- src/Algorithms/searchers/animations/bidirectionalAnim.js
+++ src/Algorithms/searchers/animations/bidirectionalAnim.js
@@ -16,11 +16,15 @@ export function bidirectionalSlow(main, sec, cellsInOrder) {
       if (i < sec.length && !sec[i].start && !sec[i].end) {
         visualizeCell("cell", "cell cell-visitedSecondary-animated", sec[i]);
       }
-      if (i < main.length && window.gridComponent.state.previousVisualization) {
-        visualizeCell("num", "num num-visited", main[i]);
-      }
-      if (i < sec.length && window.gridComponent.state.previousVisualization) {
-        visualizeCell("num", "num num-visited", sec[i]);
+      if (window.gridComponent.state.previousVisualization) {
+        if (i < main.length) {
+          if (!main[i].start) visualizeCell("num", "num num-visited", main[i]);
+          else visualizeCell("num", "num num-start", main[i]);
+        }
+        if (i < sec.length) {
+          if (!sec[i].end) visualizeCell("num", "num num-visited", sec[i]);
+          else visualizeCell("num", "num num-end", sec[i]);
+        }
       }
     }, 10 * i);
   }
@@ -38,11 +42,15 @@ export function bidirectionalFast(main, sec, cellsInOrder) {
     if (i < sec.length && !sec[i].start && !sec[i].end) {
       visualizeCell("cell", "cell cell-visitedSec", sec[i]);
     }
-    if (i < main.length && window.gridComponent.state.previousVisualization) {
-      visualizeCell("num", "num num-visited", main[i]);
-    }
-    if (i < sec.length && window.gridComponent.state.previousVisualization) {
-      visualizeCell("num", "num num-visited", sec[i]);
+    if (window.gridComponent.state.previousVisualization) {
+      if (i < main.length) {
+        if (!main[i].start) visualizeCell("num", "num num-visited", main[i]);
+        else visualizeCell("num", "num num-start", main[i]);
+      }
+      if (i < sec.length) {
+        if (!sec[i].end) visualizeCell("num", "num num-visited", sec[i]);
+        else visualizeCell("num", "num num-end", sec[i]);
+      }
     }
   }
 }
