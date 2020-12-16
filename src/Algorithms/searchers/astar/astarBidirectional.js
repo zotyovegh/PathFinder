ddcadc6fef7e87395a86a8a9248970c870a4c6aa
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-12-16 18:58:49 +0100

    Closed scenario

--- src/Algorithms/searchers/astar/astarBidirectional.js
+++ src/Algorithms/searchers/astar/astarBidirectional.js
@@ -44,70 +44,101 @@ export function astarBidirectional(
     var currentCellSec = openSetSec[currentSec];
     if (currentCellMain === endCell) {
       console.log("end found");
-      DoBidirectionalAnimation(allSetMain, allSetSec, openSetMain, openSetSec, endCell, speed)
+      DoBidirectionalAnimation(
+        allSetMain,
+        allSetSec,
+        openSetMain,
+        openSetSec,
+        endCell,
+        speed
+      );
       // DoAnimation(allSet, openSet, endCell, speed);
       return;
     }
     if (currentCellSec === startCell) {
       console.log("start found");
-      DoBidirectionalAnimation(allSetMain, allSetSec, openSetMain, openSetSec, endCell, speed)
+      DoBidirectionalAnimation(
+        allSetMain,
+        allSetSec,
+        openSetMain,
+        openSetSec,
+        startCell,
+        speed
+      );
       // DoAnimation(allSet, openSet, endCell, speed);
       return;
     }
-    eliminateFromSet(openSetMain, currentCellMain);
-    eliminateFromSet(openSetSec, currentCellSec);
-    var neighborsMain = currentCellMain.neighbors;
-    var neighborsSec = currentCellSec.neighbors;
-    for (let k = 0; k < neighborsMain.length; k++) {
-      var neighborMain = neighborsMain[k];
-      if (!(neighborMain.isWall && !neighborMain.start && !neighborMain.end)) {
-        var tentative_gScoreMain =
-          currentCellMain.g + dScore(neighborMain, currentCellMain, optimized);
-        if (tentative_gScoreMain < neighborMain.g) {
-          cameFromMain.push(neighborMain);
-          neighborMain.g = tentative_gScoreMain;
-          neighborMain.h = heuristic(
-            neighborMain,
-            endCell,
-            isDiagonalOn,
-            optimized
-          );
-          neighborMain.f = neighborMain.g + neighborMain.h;
-          neighborMain.previous = currentCellMain;
-          if (!openSetMain.includes(neighborMain)) {
-            openSetMain.push(neighborMain);
-            if (speed === "slow") {
-              allSetMain.push([openSetMain.slice(0), cameFromMain.slice(0)]);
-            } else if (speed === "fast") {
-              allSetMain.push(neighborMain);
+
+    //----------------------------------------------
+    //----------------------------------------------
+    if (!!openSetMain.length) {
+      eliminateFromSet(openSetMain, currentCellMain);
+      var neighborsMain = currentCellMain.neighbors;
+
+      for (let k = 0; k < neighborsMain.length; k++) {
+        var neighborMain = neighborsMain[k];
+        if (
+          !(neighborMain.isWall && !neighborMain.start && !neighborMain.end)
+        ) {
+          var tentative_gScoreMain =
+            currentCellMain.g +
+            dScore(neighborMain, currentCellMain, optimized);
+          if (tentative_gScoreMain < neighborMain.g) {
+            cameFromMain.push(neighborMain);
+            neighborMain.g = tentative_gScoreMain;
+            neighborMain.h = heuristic(
+              neighborMain,
+              endCell,
+              isDiagonalOn,
+              optimized
+            );
+            neighborMain.f = neighborMain.g + neighborMain.h;
+            neighborMain.previous = currentCellMain;
+            if (!openSetMain.includes(neighborMain)) {
+              openSetMain.push(neighborMain);
+              if (speed === "slow") {
+                allSetMain.push([openSetMain.slice(0), cameFromMain.slice(0)]);
+              } else if (speed === "fast") {
+                allSetMain.push(neighborMain);
+              }
             }
           }
         }
       }
     }
     //----------------------------------------------
-    for (let k = 0; k < neighborsSec.length; k++) {
-      var neighborSec = neighborsSec[k];
-      if (!(neighborSec.isWall && !neighborSec.start && !neighborSec.end)) {
-        var tentative_gScoreSec =
-          currentCellSec.gSec + dScore(neighborSec, currentCellSec, optimized);
-        if (tentative_gScoreSec < neighborSec.gSec) {
-          cameFromSec.push(neighborSec);
-          neighborSec.gSec = tentative_gScoreSec;
-          neighborSec.hSec = heuristic(
-            neighborSec,
-            endCell,
-            isDiagonalOn,
-            optimized
-          );
-          neighborSec.fSec = neighborSec.gSec + neighborSec.hSec;
-          neighborSec.previous = currentCellSec;
-          if (!openSetSec.includes(neighborSec)) {
-            openSetSec.push(neighborSec);
-            if (speed === "slow") {
-              allSetSec.push([openSetSec.slice(0), cameFromSec.slice(0)]);
-            } else if (speed === "fast") {
-              allSetSec.push(neighborSec);
+    //----------------------------------------------
+    //----------------------------------------------
+    //----------------------------------------------
+    //----------------------------------------------
+    if (!!openSetSec.length) {
+      var neighborsSec = currentCellSec.neighbors;
+      eliminateFromSet(openSetSec, currentCellSec);
+
+      for (let k = 0; k < neighborsSec.length; k++) {
+        var neighborSec = neighborsSec[k];
+        if (!(neighborSec.isWall && !neighborSec.start && !neighborSec.end)) {
+          var tentative_gScoreSec =
+            currentCellSec.gSec +
+            dScore(neighborSec, currentCellSec, optimized);
+          if (tentative_gScoreSec < neighborSec.gSec) {
+            cameFromSec.push(neighborSec);
+            neighborSec.gSec = tentative_gScoreSec;
+            neighborSec.hSec = heuristic(
+              neighborSec,
+              endCell,
+              isDiagonalOn,
+              optimized
+            );
+            neighborSec.fSec = neighborSec.gSec + neighborSec.hSec;
+            neighborSec.previous = currentCellSec;
+            if (!openSetSec.includes(neighborSec)) {
+              openSetSec.push(neighborSec);
+              if (speed === "slow") {
+                allSetSec.push([openSetSec.slice(0), cameFromSec.slice(0)]);
+              } else if (speed === "fast") {
+                allSetSec.push(neighborSec);
+              }
             }
           }
         }
@@ -115,6 +146,13 @@ export function astarBidirectional(
     }
   }
   console.log("finish in the end");
-  DoBidirectionalAnimation(allSetMain, allSetSec, openSetMain, openSetSec, endCell, speed)
+  DoBidirectionalAnimation(
+    allSetMain,
+    allSetSec,
+    openSetMain,
+    openSetSec,
+    endCell,
+    speed
+  );
   // DoAnimation(allSet, openSet, endCell, speed);
 }
